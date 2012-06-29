586739b71247d53b5e63aedbb4c695b96fd05d9d
James Trask <pair+james@pivotallabs.com>
2012-06-29 08:48:02 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sammy::Application.config.secret_token = 'd43ec62368e4a5a143e349b8e01d22f7b3cda734b19850893b51ef0274dc3a8b4863a5a479ae53ecb14c66780928d8fce2c018ee960b7fde53f2a6ea5983ed1e'
